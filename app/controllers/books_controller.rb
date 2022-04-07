7a3bafbe72406795e0c897253fe505425481c2a8
suzuki798 <xiaoyelingmu949@gmail.com>
2022-04-07 09:32:06 +0000

    add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,22 @@
 class BooksController < ApplicationController
-  def new
-  end
+
 
   def create
+
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+
+
+
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
+
+
   end
 
   def show
@@ -13,4 +24,11 @@ class BooksController < ApplicationController
 
   def destroy
   end
+
+   private
+
+  def post_image_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
