c82ce0e5be0a848956c9b57e4d9647872abad2f5
suzuki798 <xiaoyelingmu949@gmail.com>
2022-04-16 08:47:47 +0000

    add

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,5 +3,9 @@ class HomesController < ApplicationController
        top
   end
 
+  def
+      abut
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show'
   devise_for :users
  root to: 'homes#top'
+ get 'abut' => 'homes#abut'
  resources :books, only: [:create, :index, :show, :destroy]
  resources :users, only: [:show, :edit, :update, :index]
 
